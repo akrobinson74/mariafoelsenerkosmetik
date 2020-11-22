dcbfbb816bd7dcbc5a2f057a3a653edf928f2ca1
Adam Robinson <akrobinson74@gmail.com>
2020-11-22 15:24:48 +0100

    still better :)

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import data from './data/site_data.json';
 import Intro from './components/Intro';
 import ServiceCategory from './components/ServiceCategory';
-import { Col, Container, Row, Table } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <ul className="ServiceCategoryRow">
+      <Intro {...data.intro} />
+      <div className="ServiceCategoryRow">
         {data.service_categories.map((category) => (
-          <li className="ServiceCategoryCol">
+          <div className="ServiceCategoryCol">
             <ServiceCategory category={category} services={data.services[category.name]} />
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
